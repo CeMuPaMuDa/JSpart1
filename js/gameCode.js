af73e12c11bb8eab83c627e92ddc87ccb1a4b0cd
CeMuPaMuDa <bepyc9i@gmail.com>
2021-03-07 13:55:30 +0200

    task#2 game optimization

---
+++ js/gameCode.js
@@ -0,0 +1,78 @@
+//После игры необходимо спросить номер вопроса.
+//По номеру вопроса нужно вывести текст вопроса и текст выбранного ответа
+
+var event, ok;
+
+//var answers = [];
+stepNumber(quest[0]);
+
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        stepNumber(quest[1]);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                stepNumber(quest[3]);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                stepNumber(quest[3]);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert("Ошибка");
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        stepNumber(quest[2]);
+        switch (event) {
+            case 1: // Второе действие
+                stepNumber(quest[3]);
+
+                break;
+            case 2: // Второе действие
+                stepNumber(quest[3]);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert("Ошибка");
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert("Ошибка");
+}
+alert("Спасибо за игру");
+
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert("Вы ввели недопустимый символ");
+        return false;
+    } else if (event < 1 || event > 2) {
+        alert("Ваше число выходит из допустимого диапозона");
+        return false;
+    }
+    return true;
+}
+
+function havePrompt(n) {
+    return n.question + n.answer1 + n.answer2 + "-1 - Выход из игры";
+}
+
+function stepNumber(p) {
+    do {
+        ok = false;
+        event = +prompt(havePrompt(p));
+
+        if (event == -1) {
+            break;
+        } else {
+            ok = isAnswer(2, event);
+        }
+    } while (!ok);
+}
