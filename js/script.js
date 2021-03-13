638a6451686f7497509b34b85d4232bc3946d5aa
CeMuPaMuDa <bepyc9i@gmail.com>
2021-03-14 01:05:51 +0200

    task 2 & 3 complete

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
-var board = document.querySelector(".board");
-var boardLetters = document.querySelector(".letters");
-var boardNumbers = document.querySelector(".numbers");
-var boardNumbersRotate = document.querySelector(".rotate_numbers");
-var boardLettersRotate = document.querySelector(".rotate_letters");
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
-var black = false;
-var index = 0;
+var black = true;
 
 //рисуем буквы и цифры
 function numLet(p1, p2) {
-    var num = 1;
+    var num = 8;
     for (var j = 0; j < 8; j++) {
         var letter = document.createElement("li");
         letter.textContent = letters[j];
         p1.appendChild(letter);
         var numbers = document.createElement("li");
-        numbers.textContent = num++;
+        numbers.textContent = num--;
         p2.appendChild(numbers);
     }
 }
 
 //Рисуем доску
-function drawBoard(p) {
-    for (var i = 1; i <= 64; i++) {
-        var block = document.createElement("div");
-        if (black) {
-            block.classList.add("block");
-            block.classList.add("black");
-            index++;
+function drawBoard(p, text) {
+    for (var i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if (j == 0) {
+                black = !black;
+            }
+            var block = document.createElement("div");
+            if (black) {
+                block.classList.add("block");
+                block.classList.add("black");
+            } else {
+                block.classList.add("block");
+                block.classList.add("white");
+            }
+            if (text[i] != undefined && text[i][j] != undefined) {
+                block.innerHTML = text[i][j];
+            }
+
+            p.appendChild(block);
             black = !black;
-        } else {
-            block.classList.add("block");
-            block.classList.add("white");
-            index++;
-            black = !black;
-        }
-        p.appendChild(block);
-        if (index == 8) {
-            black = !black;
-            index = 0;
         }
     }
 }
-numLet(boardLetters, boardNumbers);
-numLet(boardLettersRotate, boardNumbersRotate);
-drawBoard(board);
