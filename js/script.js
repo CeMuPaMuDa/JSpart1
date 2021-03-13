3e010ca08aafef26b180e01fb962bd661070c1b2
CeMuPaMuDa <bepyc9i@gmail.com>
2021-03-13 18:10:22 +0200

    task1 complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-var board1 = document.querySelector(".board1");
-var board2 = document.querySelector(".board2");
-var board3 = document.querySelector(".board3");
+var board = document.querySelector(".board");
+var boardLetters = document.querySelector(".letters");
+var boardNumbers = document.querySelector(".numbers");
+var boardNumbersRotate = document.querySelector(".rotate_numbers");
+var boardLettersRotate = document.querySelector(".rotate_letters");
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 var black = false;
 var index = 0;
 
+//рисуем буквы и цифры
+function numLet(p1, p2) {
+    var num = 1;
+    for (var j = 0; j < 8; j++) {
+        var letter = document.createElement("li");
+        letter.textContent = letters[j];
+        p1.appendChild(letter);
+        var numbers = document.createElement("li");
+        numbers.textContent = num++;
+        p2.appendChild(numbers);
+    }
+}
+
+//Рисуем доску
 function drawBoard(p) {
     for (var i = 1; i <= 64; i++) {
         var block = document.createElement("div");
@@ -25,6 +42,6 @@ function drawBoard(p) {
         }
     }
 }
-drawBoard(board1);
-drawBoard(board2);
-drawBoard(board3);
+numLet(boardLetters, boardNumbers);
+numLet(boardLettersRotate, boardNumbersRotate);
+drawBoard(board);
