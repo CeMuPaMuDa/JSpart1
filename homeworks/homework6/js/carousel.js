fbcde45305f698ee570724e58f239b2a1512e880
CeMuPaMuDa <bepyc9i@gmail.com>
2021-03-22 17:02:52 +0200

    almost works

--- homeworks/homework6/js/carousel.js
+++ homeworks/homework6/js/carousel.js
@@ -1,4 +1,5 @@
 var sliders = document.querySelector(".slider");
+var begin = 0;
 
 for (i = 1; i <= 5; i++) {
     var image = document.createElement("img");
@@ -6,3 +7,21 @@ for (i = 1; i <= 5; i++) {
     image.alt = "slide" + i;
     sliders.appendChild(image);
 }
+
+var next = document.querySelector("#next-btn");
+next.addEventListener("click", function() {
+    begin += 1280;
+    if (begin > 5120) {
+        begin = 0;
+    }
+    sliders.style.left = -begin + "px";
+});
+
+var prev = document.querySelector("#prev-btn");
+prev.addEventListener("click", function() {
+    begin -= 1280;
+    if (begin < 0) {
+        begin = 5120;
+    }
+    sliders.style.left = -begin + "px";
+});
